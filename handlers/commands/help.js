84568bbbc1109b014e9fa705cd3df1add5128240
Novus <66804269+novusthedev@users.noreply.github.com>
2021-02-18 16:16:11 -0600

    Add files via upload

--- handlers/commands/help.js
+++ handlers/commands/help.js
@@ -19,11 +19,11 @@ module.exports = {
             {name: 'Ping (ping)', value: 'gets current ping in ms'},
             {name: 'Hackban (hackban)', value: "bans user that aren't in the server. Useful to avoid ban evasion. "},
             {name: '``` ```', value: 'Info:'},
-            {name: "Novus's Bot JS", value: 'This bot is getting FULLY rewritten but theres bugs all over the place. submit feedback on our discord server.'},
+            {name: `Novus's Bot JS`, value: 'This bot is getting FULLY rewritten but theres bugs all over the place. submit feedback on our discord server.'},
             {name: 'Discord Server', value: 'join our server at https://discord.gg/4n7Wwqz'},
             {name: 'GitHub Repository', value: 'fork the bot on GitHub https://github.com/novusthedev/novus-bot'}
             )
-        .setFooter(`prefix: ?`);
+        .setFooter(`Prefix: ?`);
         message.channel.send(newEmbed)
     }
 }
--- handlers/commands/ping.js
+++ handlers/commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
     async run(message, args, client) {
         message.channel.send("if you see this for a long time something is sus goin on").then(msg => {
             const ping = msg.createdTimestamp - message.createdTimestamp;
-            msg.edit(`Ping Pong! ping score is: ${ping}`);
+            msg.edit(`Ping: ${ping}ms`);
         });
     }
 }
