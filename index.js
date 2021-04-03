4d0b700bef17c5f3b8df6b633ab02d58deac5113
Novus <66804269+novusthedev@users.noreply.github.com>
2021-04-02 19:10:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// replace "process.env.token" with token if you are not planning to host on heroku
+// replace "process.env.token" with token if you are not planning to host on heroku //
+// DO NOT MODIFY ANYTHING ELSE IN HERE OR THE BOT MIGHT BREAK //
 
 const Discord = require("discord.js");
 Discord.Constants.DefaultOptions.ws.properties.$browser = "Discord Android"
@@ -7,14 +8,18 @@ const {
     prefix,
     token
 } = require('./conf/config.json');
+const {
+    version
+} = require('./package.json');
 
 const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log('Novus Bot Status: Online');
+    console.log(`Version: ${version}`);
     command_handler.initCommands(client);
-    client.user.setActivity('on 2.0.0A2B', { type: 'WATCHING' })
-  .then(presence => console.log(`Novus Bot Version ${presence.activities[0].name}. Hold CTRL + C to shut down the bot.`))
+    client.user.setActivity(`${prefix}help (${version})`, { type: 'WATCHING' })
+  .then(presence => console.log(`Bot presence: ${presence.activities[0].name}. Hold CTRL + C to shut down the bot.`))
   .catch(console.error);
 });
 
